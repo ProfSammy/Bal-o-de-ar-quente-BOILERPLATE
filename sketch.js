ca3074635cf6c03d23994dfc3a5692730647afd7
ProfSammy <87666814+ProfSammy@users.noreply.github.com>
2023-04-03 15:16:18 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+
+function preload(){
+
+}
+
+function setup(){
+
+//imagem de plano de fundo
+
+
+//criando canto superior e inferior
+
+      
+//criando o balão     
+
+
+
+
+}
+
+function draw() {
+  
+  background("black");
+        
+  //fazendo o balão de ar quente pular
+          
+
+  //adicionando gravidade
+
+        
+}
